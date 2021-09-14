3c74b9bb907dbd98b3861265fc2c3e5617526b9e
lwj <395901976@qq.com>
2021-09-14 19:09:39 +0800

    将toolbar插入进status并透明化statusbar

--- app/src/main/java/com/example/module_main/MainActivity.kt
+++ app/src/main/java/com/example/module_main/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.module_main
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
@@ -29,9 +34,31 @@ class MainActivity : AppCompatActivity() {
     }*/
 
 
+    protected fun setStatusBarFullTransparent() {
+        if (Build.VERSION.SDK_INT >= 21) { //21表示5.0
+            val window = window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        } else if (Build.VERSION.SDK_INT >= 19) { //19表示4.4
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            //虚拟键盘也透明
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        setStatusBarFullTransparent()
+
+
+
+
 
         val binding : ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
